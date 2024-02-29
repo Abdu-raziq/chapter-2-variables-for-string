4cb8c88035774de6bfa002a5d4700c2ec38bf282
Abdul Raziq <154526495+Abdu-raziq@users.noreply.github.com>
2024-02-29 22:21:06 +0500

    Add files via upload

---
+++ variable-for-string/app.js
@@ -0,0 +1,29 @@
+var user_name;
+
+let my_name="Abdul Raziq";
+console.log(my_name);
+
+let message="Hello world!!!";
+alert(message);
+
+alert(my_name);
+
+let my_age="22 years old";
+alert(my_age);
+
+let my_education="Certified Mobile Application Development";
+alert(my_education);
+
+let pizza_1="Pizza\nPizz\nPiz\nPi\nP";
+alert(pizza_1);
+
+const Email="My email address is RAZIQ5182@GMAIL.COM";
+alert(Email);
+
+let Book="I am trying to learn from the book A smarter \nway to learn JavaScript";
+alert(Book);
+
+document.write("Yah! I can write HTML content through Javascript");
+
+let Special ="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(Special);
